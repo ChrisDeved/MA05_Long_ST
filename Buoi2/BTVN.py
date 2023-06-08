d4101636ca5d25167e44c545e9c72d5b77c3da0c
your name <ngoclongworkspace2010@gmail.com>
2023-06-08 18:20:12 +0700

    Commit BTVN/Buoi2

---
+++ Buoi2/BTVN.py
@@ -0,0 +1,12 @@
+first_name = "Nguyễn"
+second_name = "Ngọc" 
+last_name = "Long"
+class_name = "7A1"
+age = "13"
+school = "THCS_THPT Hồng Hà"
+phone_number = "0796087482"
+print("Họ và tên:",first_name,second_name,last_name)
+print("Lớp:",class_name)
+print("Tuổi:",age)
+print("Học trường:",school)
+print("Số điện thoại:",phone_number)

16cd490ea98ed830a648572a702a92072e236cd2
your name <ngoclongworkspace2010@gmail.com>
2023-06-10 13:43:31 +0700

    Commit Buoi2

---
+++ Buoi2/Buoi_2.py
@@ -0,0 +1,11 @@
+i = 256
+x = 3.68494995
+name = "Nguyễn Ngọc Long"
+classname = "7A1"
+age = "12"
+score = "10.0"
+
+print ("Họ và tên:",name)
+print ("Lớp:",classname)
+print ("Tuổi:",age)
+print ("Điểm TB:",score)
